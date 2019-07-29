2aadafb3417f585c65011429b181c278e7d41337
Jean Ibarz <ibarz.jean@gmail.com>
2019-07-30 00:30:16 +0200

    Changed gain related methods but still having troubles sometimes.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -19,16 +19,16 @@
                 <button @click="decFreq" class="button special">&#9654; Decrease freq.</button>
             </li>
             <li id="inc_gain">
-                <button @click="incGain" class="button special">&#9654; Increase gain</button>
+                <button @click="incGainLeft(1.0); incGainRight(1.0)" class="button special">&#9654; Increase gain</button>
             </li>
             <li id="dec_gain">
-                <button @click="decGain" class="button special">&#9654; Decrease gain</button>
+                <button @click="incGainLeft(-1.0); incGainRight(-1.0)" class="button special">&#9654; Decrease gain</button>
             </li>
             <li id="bal_left">
-                <button @click="balLeft" class="button special">&#9654; Balance left</button>
+                <button @click="incGainLeft(0.5); incGainRight(-0.5)" class="button special">&#9654; Balance left</button>
             </li>
             <li id="bal_right">
-                <button @click="balRight" class="button special">&#9654; Balance right</button>
+                <button @click="incGainLeft(-0.5); incGainRight(0.5)" class="button special">&#9654; Balance right</button>
             </li>
         </ul>
     </header>
@@ -73,14 +73,16 @@ var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 var noiseLeft = audioCtx.createPinkNoise();
 var noiseRight = audioCtx.createPinkNoise();
 var merger = audioCtx.createChannelMerger(2);
-var gainNode = audioCtx.createGain(0);
+var gainNodeLeft = audioCtx.createGain(0);
+var gainNodeRight = audioCtx.createGain(0);
 var biquadFilter = audioCtx.createBiquadFilter("bandpass", 0, 1.0);
 
 // connect the nodes together
-noiseLeft.connect(merger, 0, 0);
-noiseRight.connect(merger, 0, 1);
-merger.connect(gainNode);
-gainNode.connect(biquadFilter);
+noiseLeft.connect(gainNodeLeft);
+noiseRight.connect(gainNodeRight);
+gainNodeLeft.connect(merger, 0, 0);
+gainNodeRight.connect(merger, 0, 1);
+merger.connect(biquadFilter);
 biquadFilter.connect(audioCtx.destination);
 
 // Manipulate the Biquad filter
@@ -88,8 +90,10 @@ biquadFilter.Q.setValueAtTime(20, audioCtx.currentTime);
 
 export default {
     name: "Home",
-    data: function() {
+    data() {
         let frequencies = [20, 25, 31, 40, 50, 63, 80, 100, 125, 160, 200, 250, 310, 400, 500, 630, 800, 1000, 1250, 1600, 2000, 2500, 3100, 4000, 5000, 6300, 8000, 10000, 12500, 16000, 20000]
+        let dataLeft = [0, 0, 0, -1, -2, 3, 1, 2, 2, 2, 1, -2, -4, -7, -8, -7, -8, -10, -8, -3, 1, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0]
+        let dataRight = [0, 0, 1, 0, -1, 2, 2, 2, 2, 2, 1, -2, -4, -5, -5, -4, -3, -6, -5, -3, 1, 1, 3, -1, 0, 0, 0, 0, 0, 0, 0]
         return {
             title: 'Equal Loudness Contour Estimation',
             abstract: 'A simple program to estimate your individual Equal Loudness Contour.',
@@ -97,6 +101,8 @@ export default {
             is_test_noise: true,
             frequencies: frequencies,
             frequenciesLength: frequencies.length,
+            dataLeft: dataLeft,
+            dataRight: dataRight,
             refFreqIndex: 14,
             testFreqIndex: 16,
             graph_width: 200,
@@ -109,27 +115,35 @@ export default {
                         text: "Frequency (Hz)"
                     },
                     categories: frequencies,
-                    name: 'Hz'
+                    name: 'Hz',
                 },
                 yaxis: {
                     title: {
                         text: "Amplitude (dB)"
                     },
+                    min: -50,
+                    max: 0,
                 },
+                legend: {
+                    position: 'top',
+                },
+                trigger: 0,
             },
             series: [{
                 name: 'left',
-                data: [0, 0, 0, -1, -2, 3, 1, 2, 2, 2, 1, -2, -4, -7, -8, -7, -8, -10, -8, -3, 1, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0],
+                data: dataLeft,
                 trigger: 0,
             }, {
                 name: 'right',
-                data: [0, 0, 1, 0, -1, 2, 2, 2, 2, 2, 1, -2, -4, -5, -5, -4, -3, -6, -5, -3, 1, 1, 3, -1, 0, 0, 0, 0, 0, 0, 0],
+                data: dataRight,
                 trigger: 0,
             }]
         }
     },
     methods: {
         play() {
+            this.is_playing_noise = true
+
             if (this.is_test_noise) {
                 this.playRefNoise();
             }
@@ -144,61 +158,104 @@ export default {
         incFreq() {
             this.testFreqIndex += 1;
             if (this.testFreqIndex >= this.frequenciesLength) this.testFreqIndex = 0;
-            this.updateTestNoiseFreq();
+            if (this.is_playing_noise) {
+                this.playTestNoise();
+            }
         },
         decFreq() {
             this.testFreqIndex -= 1;
             if (this.testFreqIndex < 0) this.testFreqIndex = this.frequenciesLength - 1;
-            this.updateTestNoiseFreq();
+            if (this.is_playing_noise) {
+                this.playTestNoise();
+            }
         },
-        incGain() {
-            this.series[0].data[this.testFreqIndex] += 1.0
-            this.series[1].data[this.testFreqIndex] += 1.0
-            this.series[0].trigger += 1
-            this.series[1].trigger += 1
-            // console.log("new value:"+this.series[0].data[this.testFreqIndex])
+        incGain(canal, value) {
+            var freqIndex
+            if (this.is_test_noise) {
+                freqIndex = this.testFreqIndex
+            }
+            else {
+                freqIndex = this.refFreqIndex
+            }
+            this.series[canal].data[freqIndex] += value
         },
-        decGain() {
-            this.series[0].data[this.testFreqIndex] -= 1.0
-            this.series[1].data[this.testFreqIndex] -= 1.0
+        incGainLeft(value) {
+            this.incGain(0, value)
             this.series[0].trigger += 1
-            this.series[1].trigger += 1
+            this.updateNoise();
         },
-        balLeft() {
-            this.series[0].data[this.testFreqIndex] += 0.5
-            this.series[1].data[this.testFreqIndex] -= 0.5
-            this.series[0].trigger += 1
+        incGainRight(value) {
+            this.incGain(1, value)
             this.series[1].trigger += 1
-        },
-        balRight() {
-            this.series[0].data[this.testFreqIndex] -= 0.5
-            this.series[1].data[this.testFreqIndex] += 0.5
-            this.series[0].trigger += 1
-            this.series[1].trigger += 1
+            this.updateNoise();
         },
         playTestNoise() {
-            //console.log('Test noise')
-            biquadFilter.frequency.setValueAtTime(this.frequencies[this.testFreqIndex], audioCtx.currentTime);
-            gainNode.gain.setValueAtTime(1.0, audioCtx.currentTime);
-            this.is_playing_noise = true
             this.is_test_noise = true;
+
+            biquadFilter.frequency.setValueAtTime(this.frequencies[this.testFreqIndex], audioCtx.currentTime);
+            gainNodeLeft.gain.setValueAtTime(this.gainLeft, audioCtx.currentTime);
+            gainNodeRight.gain.setValueAtTime(this.gainRight, audioCtx.currentTime);
         },
         playRefNoise() {
-            //console.log('Ref noise')
-            biquadFilter.frequency.setValueAtTime(this.frequencies[this.refFreqIndex], audioCtx.currentTime);
-            gainNode.gain.setValueAtTime(1.0, audioCtx.currentTime);
-            this.is_playing_noise = true
             this.is_test_noise = false;
+
+            biquadFilter.frequency.setValueAtTime(this.frequencies[this.refFreqIndex], audioCtx.currentTime);
+            gainNodeLeft.gain.setValueAtTime(this.gainLeft, audioCtx.currentTime);
+            gainNodeRight.gain.setValueAtTime(this.gainRight, audioCtx.currentTime);
         },
-        updateTestNoiseFreq() {
-            if(this.is_playing_noise) {
-                this.playTestNoise()
+        updateNoise() {
+            if (this.is_playing_noise) {
+                if (this.is_test_noise) {
+                    this.playTestNoise()
+                }
+                else {
+                    this.playRefNoise()
+                }
+            }
+        },
+    },
+    computed: {
+        freqIndex() {
+            if (this.is_test_noise) {
+                return this.testFreqIndex }
+            else {
+                return this.refFreqIndex }
+        },
+        gainLeft() {
+            this.series[0].trigger
+            return Math.pow(10.0, (this.dataLeft[this.freqIndex])/20.0)
+        },
+        gainRight() {
+            this.series[1].trigger
+            return Math.pow(10.0, (this.dataRight[this.freqIndex])/20.0)
+        },
+        playingFreq() {
+            if (is_playing_noise) {
+                if (is_test_noise) {
+                    return this.frequencies[this.testFreqIndex]
+                }
+                else {
+                    return this.frequencies[this.refFreqIndex]
+                }
+            }
+            else {
+                return -1
+            }
+        },
+    },
+    mounted() {
+        console.log('mounted')
+        this.options.xaxis.labels = {
+            formatter: function(value) {
+                if (value == playingFreq) {
+                    return '<<< ' + value + '>>>'
+                }
+                else {
+                    return value + 'Hz'
+                }
             }
         }
-    /* computed: {
-        frequenciesLength() {
-            return this.frequencies.length
-        } */
+        this.options.trigger += 1
     }
 }
 </script>
